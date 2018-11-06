d226e0f826c8faa3c871a7fe94d39a914037ee86
Oluwafemi Adeniji <femicodes@gmail.com>
2018-11-06 12:41:58 +0100

    added PATCH /todo/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
-let express = require('express');
-let bodyParser = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
+const _ = require('lodash');
 
 let {mongoose} = require('./db/mongoose');
 let {Todo} = require('./models/todo');
@@ -66,6 +67,33 @@ app.delete('/todos/:id', (req, res) => {
     }).catch((e) => {
       res.status(400).send();
     });
+  });
+
+  app.patch('/todos/:id', (req, res) => {
+      let id = req.params.id;
+      let body = _.pick(req.body, ['text', 'completed']);
+
+      if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+      }
+
+      if(_.isBoolean(body.completed) && body.completed) {
+          body.completedAt = new Date().getTime();
+      } else {
+          body.completed = false;
+          body.completedAt = null;
+      }
+
+      Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+        if (!todo) {
+            return res.status(400).send();
+        }
+
+        res.send({todo});
+
+      }).catch(e => {
+          res.status(400).send();
+      })
   });
 
 app.listen(port, () => {
